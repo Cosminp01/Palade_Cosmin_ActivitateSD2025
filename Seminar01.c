858f7c675b59fd400e2e53740be93d25fc86054b
Cosminp01 <CosminPalade2014@gmail.com>
2025-03-05 21:14:11 +0200

    Citirea unui nr intreg

--- Seminar01.c
+++ Seminar01.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello world!\n");
+	printf("Salutare!\n Introduceti un nr intreg:");
+	int variabila = 0;
+	scanf_s("%d", &variabila);
+	printf("Ai introdus: %d", variabila);
 	return 0;
+
 }
